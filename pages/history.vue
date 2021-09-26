243ac740e9f369d2a5f22e643fa119852fcea1c1
deathart <deathart@hotmail.fr>
2021-09-26 18:51:10 +0200

    fix mobile history

--- pages/history.vue
+++ pages/history.vue
@@ -11,9 +11,8 @@
     </div>
     <div class="min-h-screen flex justify-center">
       <div class="md:w-2/3 w-full mx-auto">
-        <div class="flex flex-row w-full">
-          <!-- left col -->
-          <div class="md:w-2/5 w-full px-2 py-10">
+        <div class="flex flex-col md:flex-row w-full">
+          <div class="md:w-2/5 w-full px-2 py-10 md:order-1 order-2">
             <div class="flex flex-col w-full bg-gray-600 rounded-lg shadow px-4 py-5">
               <div class="mb-2 flex justify-between">
                 <div class="font-bold">
@@ -34,38 +33,23 @@
               </div>
             </div>
           </div>
-          <!--line column-->
-          <div class="md:w-1/5 flex justify-center sm:hidden w-0">
+          <div class="w-1/5 justify-center flex md:order-2 order-1 mx-auto">
             <div class="relative flex h-full w-1 bg-gray-600 items-center justify-center">
-              <div class="absolute flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin md:flex hidden">
+              <div class="absolute flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin flex">
                 <div>Juin 2019</div>
                 <div>Maintenant</div>
               </div>
             </div>
           </div>
-          <!--right column-->
-          <div class="md:w-2/5 px-2 py-10 w-0">
+          <div class="w-2/5 px-2 py-10 order-3 hidden md:block">
           </div>
         </div>
-        <div class="flex flex-row w-full">
-          <!-- left col -->
-          <div class="w-2/5 px-2 py-10">
-          </div>
-          <!--line column-->
-          <div class="w-1/5  flex justify-center">
-            <div class="relative flex h-full w-1 bg-gray-600 items-center justify-center">
-              <div class="absolute flex flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin">
-                <div>Mars 2018</div>
-                <div>Juin 2019</div>
-              </div>
-            </div>
-          </div>
-          <!--right column-->
-          <div class="w-2/5 px-2 py-10 ">
-            <div class="flex flex-col w-full rounded-lg shadow bg-gray-600 px-4 py-5">
+        <div class="flex flex-col md:flex-row w-full md:mt-0 mt-14">
+          <div class="md:w-2/5 w-full px-2 py-10 md:order-3 order-2">
+            <div class="flex flex-col w-full bg-gray-600 rounded-lg shadow px-4 py-5">
               <div class="mb-2 flex justify-between">
                 <div class="font-bold">
-                  Cerise Media (Back-End)
+                  Cerise Media (Front-End)
                 </div>
               </div>
               <div class="">
@@ -81,6 +65,16 @@
                 </ul>
               </div>
             </div>
+          </div>
+          <div class="w-1/5 justify-center flex md:order-2 order-1 mx-auto">
+            <div class="relative flex h-full w-1 bg-gray-600 items-center justify-center">
+              <div class="absolute flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin flex">
+                <div>Juin 2019</div>
+                <div>Maintenant</div>
+              </div>
+            </div>
+          </div>
+          <div class="w-2/5 px-2 py-10 order-1 hidden md:block">
           </div>
         </div>
       </div>
