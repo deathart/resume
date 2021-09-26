7033e61fd9c79013e8f5d1d06a97b939bc7c7269
deathart <deathart@hotmail.fr>
2021-09-26 16:39:56 +0200

    typo history

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -5,7 +5,7 @@
         <img src="~/assets/images/pic.jpg" class="rounded-lg shadow-lg">
       </figure>
       <div class="max-w-md card-body">
-        <h2 class="card-title">BRABANT Louis</h2>
+        <h2 class="card-title">A propos de moi</h2>
         <p>
           Je suis autodidacte depuis toujours, je suis passionné par l'informatique, mais surtout la programmation Web, j'ai beaucoup appris via divers sites comme openclassroom (Anciennement siteduzero.com), mais aussi via des projets de jeux (World of Warcraft, Minecraft, Dofus).
           Je m'amuse souvent à développer des petits programmes (NODE.JS - Electron) pour me simplifier la vie (Exemple: actualisation automatique de pôle emploi Gestionnaire de tâches automatisé) !
--- pages/history.vue
+++ pages/history.vue
@@ -10,10 +10,10 @@
       </div>
     </div>
     <div class="min-h-screen flex justify-center">
-      <div class="w-2/3 mx-auto">
+      <div class="md:w-2/3 w-full mx-auto">
         <div class="flex flex-row w-full">
           <!-- left col -->
-          <div class="w-2/5 px-2 py-10">
+          <div class="md:w-2/5 w-full px-2 py-10">
             <div class="flex flex-col w-full bg-gray-600 rounded-lg shadow px-4 py-5">
               <div class="mb-2 flex justify-between">
                 <div class="font-bold">
@@ -35,16 +35,16 @@
             </div>
           </div>
           <!--line column-->
-          <div class="w-1/5  flex justify-center">
+          <div class="md:w-1/5 flex justify-center sm:hidden w-0">
             <div class="relative flex h-full w-1 bg-gray-600 items-center justify-center">
-              <div class="absolute flex flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin">
+              <div class="absolute flex-col justify-center h-24 w-24 rounded-full border-2 border-gray-600 leading-none text-center z-10 bg-base-200 font-thin md:flex hidden">
                 <div>Juin 2019</div>
                 <div>Maintenant</div>
               </div>
             </div>
           </div>
           <!--right column-->
-          <div class="w-2/5 px-2 py-10 ">
+          <div class="md:w-2/5 px-2 py-10 w-0">
           </div>
         </div>
         <div class="flex flex-row w-full">
